4faacd342a267ad9e2a144bca3ef9ffa563948a1
Swarajkoppu <141053256+Swarajkoppu@users.noreply.github.com>
2024-05-02 14:04:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var currentsong = new Audio()
 let songs = []
 let currfolder;
-const token = `${{ secrets.MY_ID }}`;
+const token = `${secrets.MY_ID }`;
 console.log(token)
 function secondsToMinutesSeconds(seconds) {
     // Calculate minutes and seconds
